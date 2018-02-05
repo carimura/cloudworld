cc21a118ec50389918c301b1b865327287c4c0a5
Chad Arimura <carimura@gmail.com>
2018-02-04 18:08:15 -0800

    deal_crawler

---
+++ deal_crawler/func.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"io"
+
+	fdk "github.com/fnproject/fdk-go"
+)
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+	// var person struct {
+	// 	Name string `json:"name"`
+	// }
+	// json.NewDecoder(in).Decode(&person)
+	// if person.Name == "" {
+	// 	person.Name = "world"
+	// }
+
+	// msg := struct {
+	// 	Msg string `json:"msg"`
+	// }{
+	// 	Msg: fmt.Sprintf("Hello %s!\n", person.Name),
+	// }
+
+	//json.NewEncoder(out).Encode(&msg)
+}
---
+++ deal_crawler/run.sh
@@ -0,0 +1,5 @@
+for i in {1..100}
+do
+   fn call cloudworld deal_crawler
+done
+
